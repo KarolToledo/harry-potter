1059c49e0c1f24b29ba73510cc94a59fb37c7098
karoltoledo <karolinatoledo2010@gmail.com>
2023-07-12 19:01:07 -0300

    Alteracao do nome das imagens e construcao do codigo de javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,37 @@
 // Esse é um comentário em JavaScript em uma linha
 /*Para comentar mais linhas use barra + asterístico*/
+ 
+/* Passo 01 */
+const personagens = document.querySelectorAll('.personagem');
 
-const personagem = document.querySelectorAll('.personagem');
+/* Passo 02 */
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450){
+            window.scrollTo({top: 0, behavior: 'smooth' });
+        }
+        
+        /* Passo 03 */
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png `;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+    })
+})
+
+/* Passo 03 */
 
 
